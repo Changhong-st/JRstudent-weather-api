5277c548aaee66b21cd9489f23913b839c11b61d
Changhong Lin <changhong.linst@gmail.com>
2020-08-15 21:15:11 +1000

    heroku deploy

--- src/models/LocationModel.js
+++ src/models/LocationModel.js
@@ -4,6 +4,7 @@ class LocationWeather extends CurrentWeather {
     constructor(init_data) {
         super(init_data);
         this.name = init_data.name;
+        this.country = init_data.sys.country;
     }
 }
 
